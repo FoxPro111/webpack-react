0c6ddf9676703faae10db411058927343afc19d8
FoxPro111 <yurec007@list.ru>
2019-04-25 23:49:30 +0300

    add support for js & jsx files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    devtools: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    resolve: {
+        extentions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
