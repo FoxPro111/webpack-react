af5975594aa3e06636195593133f39e6ebdf2460
FoxPro111 <yurec007@list.ru>
2019-04-26 21:34:31 +0300

    add settings for dev & prod

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 
+import Users from './containers/Users';
 import asyncComponent from './hoc/asyncComponent';
 
-import Users from './containers/Users';
-// import Pizza from './containers/Pizza';
-
 const AsyncPizza = asyncComponent(() => {
-    return import('./containers/Users');
+  return import('./containers/Pizza.js');
 });
 
 class App extends Component {
-    render() {
-        return (
-          <div>
-              <div>
-                <Link to="/">Users</Link>
-                <Link to="/pizza">Pizza</Link>
-              </div>
-              <div>
-                <Route path="/" exact component={Users} /> 
-                    <Route path="/pizza" component={AsyncPizza} /> 
-              </div>
-          </div>  
-        );
-    }
-};
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link>
+          <Link to="/pizza">Pizza</Link>
+        </div>
+        <div>
+          <Route path="/" exact component={Users} />
+          <Route path="/pizza" component={AsyncPizza} />
+        </div>
+      </div>
+    );
+  }
+}
 
+export default App;
